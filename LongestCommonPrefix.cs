be9918a98021897b999dee63852bfc959b23363b
kaspertreborg <kaspertreborg@outlook.com>
2023-10-11 09:58:47 +0200

    Initial commit with a bunch of easy problems

---
+++ LongestCommonPrefix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace LeetCodeProblems
+{
+	public class LongestCommonPrefix
+	{
+		public string LongestCommonPrefixFunction(string[] strs)
+		{
+			if (strs.Length == 0) return "";
+
+			string prefix = strs[0];
+			for (int i = 1; i < strs.Length; i++)
+			{
+				while (strs[i].IndexOf(prefix) != 0)
+				{
+					prefix = prefix[..^1];
+					if (string.IsNullOrEmpty(prefix)) return "";
+				}
+			}
+
+			Console.WriteLine(prefix);
+			return prefix;
+
+		}
+	}
+}
---
+++ TheMaximumAchievableX.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace LeetCodeProblems
+{
+	public class TheMaximumAchievableX
+	{
+		public int TheMaximumAchievableXFunc(int num, int t)
+		{
+
+			return num + 2*t;
+
+		}
+	}
+}
+
